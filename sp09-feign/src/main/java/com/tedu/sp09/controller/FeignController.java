cd5b3ff9a80115e6ce7fb816f8c123ed075b85d4
feal <feal@DESKTOP-8UV2N4D>
2019-11-10 19:58:06 +0800

    yf-cloud-full

---
+++ sp09-feign/src/main/java/com/tedu/sp09/controller/FeignController.java
@@ -0,0 +1,68 @@
+package com.tedu.sp09.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tedu.sp01.pojo.Item;
+import com.tedu.sp01.pojo.Order;
+import com.tedu.sp01.pojo.User;
+import com.tedu.sp09.service.ItemFeignService;
+import com.tedu.sp09.service.OrderFeignService;
+import com.tedu.sp09.service.UserFeignService;
+import com.tedu.web.util.JsonResult;
+
+@RestController
+public class FeignController {
+
+	@Autowired
+	private ItemFeignService itemService;
+	
+	@Autowired
+	private OrderFeignService orderService;
+	
+	@Autowired
+	private UserFeignService userServcie;
+
+	
+	@GetMapping("/item-service/{orderId}")
+	public JsonResult<List<Item>> getItems(@PathVariable String orderId){
+		
+		return itemService.getItems(orderId);
+		
+	}
+	
+	@PostMapping("/item-service/decreaseNumber")
+	public JsonResult decreaseNumbes(List<Item> list) {
+		                   
+		return itemService.decreaseNumbers(list);
+	}
+	
+	@GetMapping("/order-service/{orderId}")
+	public JsonResult<Order> getOrder(@PathVariable String orderId){
+		return orderService.getOrder(orderId);
+	}
+	
+	@GetMapping("/order-service")
+	public JsonResult addOrder() {
+		
+		return orderService.addOrder();
+	}
+	
+	
+	@GetMapping("/user-service/{userId}")
+	public JsonResult<User> getUser(@PathVariable Integer userId) {
+		return userServcie.getUser(userId);
+	}
+
+	@GetMapping("/user-service/{userId}/score") 
+	public JsonResult addScore(@PathVariable Integer userId, Integer score) {
+		return userServcie.addScore(userId, score);
+	}
+	
+	
+}
